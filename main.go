76688ed139001b14399e4c15578a5d7a68873169
Derek Smart <derek@grindaga.com>
2025-08-13 10:51:23 -0400

    setup http middleware

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "log"
     "net/http"
 )
@@ -20,8 +21,49 @@ func handleRequests(cmdr Commander) http.Handler {
     return mux
 }
 
+type CommandRequest struct {
+    Type    string `json:"type"`    // "ping" or "sysinfo"
+    Payload string `json:"payload"` // For ping, this is the host
+}
+
+type CommandResponse struct {
+    Success bool        `json:"success"`
+    Data    interface{} `json:"data"`
+    Error   string      `json:"error,omitempty"`
+}
+
 func handleCommand(cmdr Commander) http.HandlerFunc {
+    return middleware(func(w http.ResponseWriter, r *http.Request) {
+        log.Println(r.Method, r.URL.Path)
+        panic("testing")
+    })
+}
+
+func middleware(next http.HandlerFunc) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-        // Parse request and execute command
+        var response CommandResponse
+        defer func() {
+            // Catch all Panics and throw a 500
+            if r := recover(); r != nil {
+                log.Printf("Recovered from panic: %v\n", r)
+                response.Success = false
+                response.Error = fmt.Sprintf("%v", r)
+                w.WriteHeader(http.StatusInternalServerError)
+            }
+        }()
+        // disallow paths other than /execute
+        if r.URL.Path != "/execute" {
+            response.Success = false
+            response.Error = "invalid path"
+            w.WriteHeader(http.StatusNotFound)
+        }
+        // disallow methods other than POST
+        if r.Method != "POST" {
+            response.Success = false
+            response.Error = "invalid method"
+            w.WriteHeader(http.StatusMethodNotAllowed)
+        }
+        // pass through to handler
+        next(w, r)
     }
 }
